f10417268f87b058290bec917c3a1da4e3914bfd
yinhl <yinhl@getui.com>
2018-03-19 10:32:01 +0800

    * table组件简版

--- src/demo-app/table/table-demo.component.ts
+++ src/demo-app/table/table-demo.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+//import { SliderDemoComponent } from '../slider/slider-demo.component';
+import { ExpandDemoComponent } from './expand-demo.component';
 @Component({
   moduleId: module.id,
   selector: 'gt-table-demo',
@@ -10,30 +12,68 @@ import { Component, OnInit } from '@angular/core';
 export class TableDemoComponent implements OnInit {
   columns: any[];
   data: any[];
+  actions: any[];
+  expand: any;
+  row: any;
   constructor() {
     this.columns = [
       {
         id: 'firstName',
         field: 'firstName',
         label: '姓',
-        width: 100
+        width: '400px',
+        style: function () {
+            //  console.log(row);
+            // console.log(val);
+          return {
+            'color': 'red'
+          }
+        },
+        component: ExpandDemoComponent,
+        hidden: false
       },
       {
         id: 'lastName',
         field: 'lastName',
-        label: '名'
+        label: '名',
+        style: {
+          'color': 'red'
+        }
       }
     ];
     this.data =  [
       {
+
+        'id': 45,
         'firstName': '产',
         'lastName': '品'
+
       },
       {
+        'id': 66,
         'firstName': '开',
         'lastName': '发'
       }
     ];
+    this.actions = [
+      {
+        label: '查看',
+        click: () => {
+        }
+      }, {
+        label: '删除',
+        disabled: false,
+        confirm: '确定删除吗',
+        click: () => {} //点击后的回调
+      },  {
+        label: '任务结果',
+        hidden: false,
+        click: (row: any) => {
+          console.log(row)
+        }//点击后的回调
+      }
+    ];
+    this.expand = ExpandDemoComponent
   }
 
   ngOnInit() {
@@ -42,5 +82,11 @@ export class TableDemoComponent implements OnInit {
 
   changeData() {
     this.data.push(this.data[1]);
+  }
+  onCheckedRow(event: any) {
+    console.log(event)
+  }
+  onExpandRow(event: any) {
+    console.log(event)
   }
 }
---
+++ src/lib/table/table-cell.component.ts
@@ -0,0 +1,73 @@
+import {
+  Component,
+  ComponentFactoryResolver,
+  ComponentRef,
+  Injector,
+  Input,
+  AfterViewInit,
+  OnDestroy,
+  ViewChild,
+  ViewContainerRef
+} from '@angular/core';
+import { GtColumnOption } from './table';
+
+@Component({
+  selector: '[table-cell]',
+  template: `
+    <ng-container *ngIf="!column.component"> 
+      <span>{{ getFormattedValue() }} </span>
+    </ng-container>
+    <ng-template *ngIf="column.component" #cmpContainer></ng-template>
+  `
+})
+export class TableCellComponent implements AfterViewInit, OnDestroy {
+  /**
+   * 这一行的值
+   */
+  @Input() row: any;
+  /**
+   * 这一列的配置
+   */
+  @Input() column: GtColumnOption;
+  @ViewChild('cmpContainer', {read: ViewContainerRef}) cmpContainer: ViewContainerRef;
+
+  private _componentRef: ComponentRef<any>;
+
+  constructor( private resolver: ComponentFactoryResolver ){}
+
+  getValue(): any{
+    return this.row[this.column.field];
+  }
+
+  getFormattedValue(): any {
+    if (this.column.formatter) {
+      return this.column.formatter(this.getValue(), this.row, this.column);
+    }
+    return this.getValue();
+  }
+
+  ngAfterViewInit(): void {
+    if (this.column.component) {
+      if (this._componentRef) {
+        this._componentRef.destroy();
+      }
+      const cmpFactory = this.resolver.resolveComponentFactory(this.column.component);
+      const ctxJnjector: Injector = this.cmpContainer.injector;
+      this._componentRef = this.cmpContainer.createComponent(cmpFactory, 0, ctxJnjector);
+      const instance: any = this._componentRef.instance;
+      instance['row'] = this.row;
+      instance['column'] = this.column;
+      instance['field'] = this.column.field;
+      instance['value'] = this.getValue();
+      console.log(instance);
+      this._componentRef.changeDetectorRef.detectChanges();
+    }
+  }
+
+  ngOnDestroy(): void {
+    if (this._componentRef) {
+      this._componentRef.destroy();
+    }
+  }
+
+}
